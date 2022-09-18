403aa63b2b39223bc98f922959d32bdb041e04ba
AeonRyzor <96483827+AeonRyzor@users.noreply.github.com>
2022-09-17 23:06:15 -0400

    Outline update
    This update pushes an outline which includes all pages site wide. though the site may not be completed yet all the pages are linked so at least the navbar works

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,45 +1,44 @@
-
 var beta_modal = document.getElementById("modalBeta");
 var join_modal = document.getElementById("modalJoin");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var join_modal_close  = document.getElementsByClassName("close")[1];
+var join_modal_close = document.getElementsByClassName("close")[1];
 
 window.onload = function() {
-  beta_modal.style.display = "block";
-  beta_modal.style.animation = fade;
-  beta_modal.style.animationDuration = "1s";
+    beta_modal.style.display = "block";
+    beta_modal.style.animation = fade;
+    beta_modal.style.animationDuration = "1s";
 };
 
 span.onclick = function() {
-  beta_modal.style.display = "none";
-  beta_modal.style.animation = fade_out;
-  beta_modal.style.animationDuration = "1s";
+    beta_modal.style.display = "none";
+    beta_modal.style.animation = fade_out;
+    beta_modal.style.animationDuration = "1s";
 };
 join_modal_close.onclick = function() {
-  join_modal.style.display = "none";
-  join_modal.style.animation = fade_out;
-  join_modal.style.animationDuration = "1s";
+    join_modal.style.display = "none";
+    join_modal.style.animation = fade_out;
+    join_modal.style.animationDuration = "1s";
 
 };
 
 
 
-btn.onclick = function(){
-join_modal.style.display = "block";
-join_modal.style.animation = fade;
-join_modal.style.animationDuration = "1s";
+btn.onclick = function() {
+    join_modal.style.display = "block";
+    join_modal.style.animation = fade;
+    join_modal.style.animationDuration = "1s";
 };
 // When the user clicks the button, open the modal 
 
 window.onclick = function(event) {
-  if (event.target == beta_modal) {
-    beta_modal.style.display = "none";
-  }   else if (event.target == join_modal) {
-    join_modal.style.display = "none";
-  }
+    if (event.target == beta_modal) {
+        beta_modal.style.display = "none";
+    } else if (event.target == join_modal) {
+        join_modal.style.display = "none";
+    }
 };
 
-function closeBetaModal(){
-span.click();
-};
+function closeBetaModal() {
+    span.click();
+}
