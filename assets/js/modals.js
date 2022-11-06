4ad638c8368a4fb3e374c56c68a7f5700ed54c3a
AeonRyzor <96483827+AeonRyzor@users.noreply.github.com>
2022-11-06 14:00:02 -0500

    Beta release!
    this update creates a beta page which is only accessible through a link.
    
    great for testing out features before they go live

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,47 +1,50 @@
 var beta_modal = document.getElementById("modalBeta");
 var join_modal = document.getElementById("modalJoin");
 var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close");
-var join_modal_close = document.getElementsByClassName("close");
+var span = document.getElementsByClassName("beta_close");
+var join_modal_close = document.getElementsByClassName("join_close");
 
+if (beta_modal) {
 
-
-
-
-btn.onclick = function() {
-    join_modal.style.display = "block";
-    join_modal.style.animation = "fade";
-    join_modal.style.animationDuration = "1s";
-};
-// When the user clicks the button, open the modal 
-
-window.onclick = function(event) {
-    if (event.target == beta_modal) {
+    window.onload = function() {
+        beta_modal.style.display = "block";
+        beta_modal.style.animation = fade;
+        beta_modal.style.animationDuration = "1s";
+    };
+    span.onclick = function() {
         beta_modal.style.display = "none";
-    } else if (event.target == join_modal) {
-        join_modal.style.display = "none";
-    }
-};
-join_modal_close.onclick = function() {
-    join_modal.style.display = "none";
-    join_modal.style.animation = "fade_out";
-    join_modal.style.animationDuration = "1s";
+        beta_modal.style.animation = fade_out;
+        beta_modal.style.animationDuration = "1s";
+    };
+    window.onclick = function(event) {
+        if (event.target == beta_modal) {
+            beta_modal.style.display = "none";
+        }
+    };
 
-};
-
-function closeBetaModal() {
-    span.click();
+    closeBetaModal = function() {
+        span.click();
+    };
 }
+if (join_modal) {
+    btn.onclick = function() {
+        join_modal.style.display = "block";
+        join_modal.style.animation = "fade";
+        join_modal.style.animationDuration = "1s";
+    };
+    // When the user clicks the button, open the modal 
 
+    window.onclick = function(event) {
+        if (event.target == beta_modal) {
+            beta_modal.style.display = "none";
+        } else if (event.target == join_modal) {
+            join_modal.style.display = "none";
+        }
+    };
+    join_modal_close.onclick = function() {
+        join_modal.style.display = "none";
+        join_modal.style.animation = "fade_out";
+        join_modal.style.animationDuration = "1s";
 
-span.onclick = function() {
-    beta_modal.style.display = "none";
-    beta_modal.style.animation = fade_out;
-    beta_modal.style.animationDuration = "1s";
-};
-
-window.onload = function() {
-    beta_modal.style.display = "block";
-    beta_modal.style.animation = fade;
-    beta_modal.style.animationDuration = "1s";
-};
+    };
+}
